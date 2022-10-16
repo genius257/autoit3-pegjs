42d139d33361cb474011939b640f99089a5f2b43
Anders Pedersen <hot415@hotmail.com>
2022-10-16 02:42:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-export default require('./out/autoit3');
+//export default require('./out/autoit3');
+
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = require('./out/autoit3');
