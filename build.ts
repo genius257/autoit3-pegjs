63591640895e973cb7e197ab17584ec96fa2cda6
Anders Pedersen <hot415@hotmail.com>
2025-01-21 21:59:41 +0100

    Fixed issue with type generation for MemberExpression and CallExpression rules.
    caused by usage of array.reduce in a way that works with pure JS, but are type interpeted wrong by TypeScript, due to limited type resolving implementation of Array.resolve

--- build.ts
+++ build.ts
@@ -2,9 +2,34 @@
 
 import {TypeExtractor} from "peggy-to-ts";
 import {readFileSync, writeFileSync} from "fs";
+import {Project} from "ts-morph";
 
 const source = readFileSync("./src/autoit3.pegjs").toString();
 const typeExtractor = new TypeExtractor(source, {camelCaseTypeNames: true, removeReadonlyKeyword: true});
 typeExtractor.getTypes().then((types) => {
     writeFileSync('./out/autoit3.d.ts', "\nexport namespace AutoIt3 {\n"+types+"\n}\n", {flag: 'a+'});
+
+    const project = new Project();
+    const file = project.addSourceFileAtPath('./out/autoit3.d.ts');
+
+    const namespace = file.getModule("AutoIt3");
+    if (namespace === undefined) {
+        throw new Error('namespace "AutoIt3" not found');
+    }
+
+    const alias = namespace.getTypeAlias("MemberExpression");
+    if (alias === undefined) {
+        throw new Error('type "MemberExpression" not found in "AutoIt3" namespace');
+    }
+
+    alias.setType(`PrimaryExpression | ({type: "MemberExpression", object: MemberExpression|PrimaryExpression, location: { source: string | undefined, start: { offset: number, line: number, column: number }, end: { offset: number, line: number, column: number } } } & ({property: Expression, computed: true} | {property:IdentifierName, computed: false}))`);
+
+    const alias2 = namespace.getTypeAlias("CallExpression");
+    if (alias2 === undefined) {
+        throw new Error('type "CallExpression" not found in "AutoIt3" namespace');
+    }
+
+    alias2.setType(`{type: "CallExpression", callee: MemberExpression | CallExpression, arguments: Arguments, location: { source: string | undefined, start: { offset: number, line: number, column: number }, end: { offset: number, line: number, column: number } } } | {type: "MemberExpression", property: Expression | IdentifierName, computed: boolean, location: { source: string | undefined, start: { offset: number, line: number, column: number }, end: { offset: number, line: number, column: number } } }`);
+
+    file.saveSync();
 });
