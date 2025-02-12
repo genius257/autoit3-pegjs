ad69c8506834e7f7122a31c45b21168144d08a91
Anders Pedersen <hot415@hotmail.com>
2025-02-13 00:02:56 +0100

    Changed litteral location object to LocationRange type alias for manual type replacement properties in build.ts

--- build.ts
+++ build.ts
@@ -22,14 +22,14 @@ typeExtractor.getTypes().then((types) => {
         throw new Error('type "MemberExpression" not found in "AutoIt3" namespace');
     }
 
-    alias.setType(`PrimaryExpression | ({type: "MemberExpression", object: MemberExpression|PrimaryExpression, location: { source: string | undefined, start: { offset: number, line: number, column: number }, end: { offset: number, line: number, column: number } } } & ({property: Expression, computed: true} | {property:IdentifierName, computed: false}))`);
+    alias.setType(`PrimaryExpression | ({type: "MemberExpression", object: MemberExpression|PrimaryExpression, location: LocationRange } & ({property: Expression, computed: true} | {property:IdentifierName, computed: false}))`);
 
     const alias2 = namespace.getTypeAlias("CallExpression");
     if (alias2 === undefined) {
         throw new Error('type "CallExpression" not found in "AutoIt3" namespace');
     }
 
-    alias2.setType(`{type: "CallExpression", callee: MemberExpression | CallExpression, arguments: Arguments, location: { source: string | undefined, start: { offset: number, line: number, column: number }, end: { offset: number, line: number, column: number } } } | {type: "MemberExpression", property: Expression | IdentifierName, computed: boolean, location: { source: string | undefined, start: { offset: number, line: number, column: number }, end: { offset: number, line: number, column: number } } }`);
+    alias2.setType(`{type: "CallExpression", callee: MemberExpression | CallExpression, arguments: Arguments, location: LocationRange } | {type: "MemberExpression", property: Expression | IdentifierName, computed: boolean, location: LocationRange }`);
 
     file.saveSync();
 });
