d8df5f0531a4014c58d1c8e402f63cc0e7da0d42
Anders Pedersen <hot415@hotmail.com>
2026-01-30 07:14:51 +0100

    Fixed failing test after parser rule changes

--- tests/FunctionDeclaration.test.ts
+++ tests/FunctionDeclaration.test.ts
@@ -1,6 +1,7 @@
 import { expect, test } from 'vitest';
 import fs from 'fs';
 import parser, { AutoIt3 } from "../dist/autoit3";
+import { assertParserError } from './helpers';
 
 test('FunctionDeclaration', () => {
     expect(
@@ -11,9 +12,15 @@ test('FunctionDeclaration', () => {
         )
     ).toMatchSnapshot();
 
+    assertParserError(
+        `Func MyFunction($a, Byref Const $b = 1)
+        EndFunc`,
+        35
+    );
+
     expect(
         parser.parse(
-            `Func MyFunction($a, Byref Const $b = 1)
+            `Func MyFunction($a, Byref Const $b)
             EndFunc`,
             {grammarSource: ""}
         )
