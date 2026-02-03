genius257/autoit3-pegjs

commit ea122e3ce373cfaf626ec308d714000d36adcad7

Fixed example issue from #48

src/autoit3.pegjs
@@ -797,16 +797,11 @@ UnaryOperator
 //BUG: --------------------------------------------------------------------------------------------------------------------------------------
 
 Program
-  = body:SourceElements? {
+  = body:SourceElement* {
       return {
         type: "Program",
-        body: body??[]
+        body: body,
       };
-    }
-
-SourceElements
-  = head:SourceElement tail:(__ @SourceElement)* {
-      return [head, ...tail];
     }
 
 SourceElement

commit 55d51683f68781001eb07cc073c6d32ac523d21d

Fixed bug caused by ea122e3ce373cfaf626ec308d714000d36adcad7

src/autoit3.pegjs
@@ -1155,7 +1155,7 @@ UnaryOperator
 //BUG: --------------------------------------------------------------------------------------------------------------------------------------
 
 Program
-  = body:SourceElement* {
+  = body:SourceElement|.., __| {
       return {
         type: "Program",
         body: body,