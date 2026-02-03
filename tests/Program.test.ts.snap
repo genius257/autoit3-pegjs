genius257/autoit3-pegjs

commit 7b76a40eb3c6d991e01742eba82aabc4940be687

Fixed issues with too loose MultiLineCommentStartTag detection for nested MultiLineComment (issue #39)

src/autoit3.pegjs
@@ -142,7 +142,7 @@ SingleLineComment = ';' body:([^\u000A\u000D]*) {
 MultiLineCommentStartTag = "#" (CSToken / CommentsStartToken)
 
 MultiLineComment
-  = MultiLineCommentStartTag beforeBody:$($(!Newline .)*)? body:$((MultiLineComment/!(Newline Whitespace* MultiLineCommentEndTag) .)* Newline Whitespace*) MultiLineCommentEndTag afterBody:$($(!Newline .)*)? {
+  = MultiLineCommentStartTag beforeBody:$($(!Newline .)*)? body:$((Newline Whitespace* MultiLineComment/!(Newline Whitespace* MultiLineCommentEndTag) .)* Newline Whitespace*) MultiLineCommentEndTag afterBody:$($(!Newline .)*)? {
     return {
       type: "MultiLineComment",
       body: beforeBody+body,

commit f7579fd7d2838c455646c1cb68adbe0553793adc

Added test for bug introduced by 7b76a40

tests/Program.test.ts
@@ -1,0 +1,11 @@
+import { expect, it } from "vitest";
+import parser from "../dist/autoit3";
+
+it('whitespace between SourceElements', () => {
+    expect(
+        parser.parse(
+            `;\n ;\n\t;\n`,
+            {grammarSource: ""}
+        )
+    ).toMatchSnapshot();
+});

tests/Program.test.ts.snap
@@ -1,0 +1,60 @@
+// Vitest Snapshot v1, https://vitest.dev/guide/snapshot.html
+
+exports[`whitespace between SourceElements 1`] = `
+{
+  "body": [
+    {
+      "body": "",
+      "location": {
+        "end": {
+          "column": 2,
+          "line": 1,
+          "offset": 1,
+        },
+        "source": "",
+        "start": {
+          "column": 1,
+          "line": 1,
+          "offset": 0,
+        },
+      },
+      "type": "SingleLineComment",
+    },
+    {
+      "body": "",
+      "location": {
+        "end": {
+          "column": 3,
+          "line": 2,
+          "offset": 4,
+        },
+        "source": "",
+        "start": {
+          "column": 2,
+          "line": 2,
+          "offset": 3,
+        },
+      },
+      "type": "SingleLineComment",
+    },
+    {
+      "body": "",
+      "location": {
+        "end": {
+          "column": 3,
+          "line": 3,
+          "offset": 7,
+        },
+        "source": "",
+        "start": {
+          "column": 2,
+          "line": 3,
+          "offset": 6,
+        },
+      },
+      "type": "SingleLineComment",
+    },
+  ],
+  "type": "Program",
+}
+`;