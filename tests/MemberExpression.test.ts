f7521233e669bfc164f6a8fdbcd9e46a7069ae0d
Anders Pedersen <hot415@hotmail.com>
2026-02-04 08:07:58 +0100

    Added memberexpressions test assertions to include in with statements

--- tests/MemberExpression.test.ts
+++ tests/MemberExpression.test.ts
@@ -5,15 +5,25 @@ import { assertParserError } from "./helpers";
 it('dot notation whitespaces', () => {
     expect(parser.parse(`$o.a`, {grammarSource: ""})).toMatchSnapshot();
 
+    expect(parser.parse(`With $o\n.a\nEndWith`, {grammarSource: ""})).toMatchSnapshot();
+    
     expect(parser.parse(`$o.a.b`, {grammarSource: ""})).toMatchSnapshot();
+
+    expect(parser.parse(`With $o\n.a.b\nEndWith`, {grammarSource: ""})).toMatchSnapshot();
 
     expect(parser.parse(`$o .a`, {grammarSource: ""})).toMatchSnapshot();
 
     expect(parser.parse(`$o .a .b`, {grammarSource: ""})).toMatchSnapshot();
 
+    expect(parser.parse(`With $o\n.a .b\nEndWith`, {grammarSource: ""})).toMatchSnapshot();
+
     expect(assertParserError(`$o. a`, 3)).toMatchSnapshot();
 
+    expect(assertParserError(`With $o\n. a\nEndWith`, 9)).toMatchSnapshot();
+
     expect(assertParserError(`$o.a. b`, 5)).toMatchSnapshot();
+
+    expect(assertParserError(`With $o\n.a. b\nEndWith`, 11)).toMatchSnapshot();
 
     expect(parser.parse(`f().a`, {grammarSource: ""})).toMatchSnapshot();
 
