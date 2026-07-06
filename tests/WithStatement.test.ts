249b1bedad73ec8c3639fd5aa21a3914b4c006b4
Anders Pedersen <hot415@hotmail.com>
2026-07-06 06:24:23 +0200

    Added test for latest bug fix

--- tests/WithStatement.test.ts
+++ tests/WithStatement.test.ts
@@ -5,3 +5,7 @@ import { assertParserError } from './helpers';
 test('WithStatementTest', () => {
     expect(parser.parse(`With $o\nIf .a = .b Then Return 1\nEndWith`, {grammarSource: ""})).toMatchSnapshot();
 });
+
+test('IfStatementInWith with multiple statements', () => {
+    expect(parser.parse(`With $o\nIf .a = .b Then\n    .c = 1\n    .d = 2\nEndIf\nEndWith`, {grammarSource: ""})).toMatchSnapshot();
+});
